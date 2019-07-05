db6642ef173bc86e05e218a70fa24d9e679773dc
Bill Farmer <williamjfarmer@yahoo.co.uk>
2019-07-05 18:43:58 +0100

    Update sources

--- src/main/java/org/billthefarmer/notes/Notes.java
+++ src/main/java/org/billthefarmer/notes/Notes.java
@@ -24,6 +24,20 @@ package org.billthefarmer.notes;
 import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.ScrollView;
+import android.widget.SearchView;
+import android.widget.ViewSwitcher;
+
+import android.support.v4.content.FileProvider;
+
+import org.billthefarmer.markdown.MarkdownView;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Notes extends Activity
 {
@@ -31,6 +45,16 @@ public class Notes extends Activity
     public final static String CHANGED = "changed";
     public final static String CONTENT = "content";
     public final static String PREF_DARK = "dark";
+
+    private EditText textView;
+    private ScrollView scrollView;
+
+    private MarkdownView markdownView;
+    private ViewSwitcher layoutSwitcher;
+    private ViewSwitcher buttonSwitcher;
+
+    private SearchView searchView;
+    private MenuItem searchItem;
 
     // onCreate
     @Override
@@ -49,5 +73,387 @@ public class Notes extends Activity
             setTheme(R.style.AppDarkTheme);
 
         setContentView(R.layout.editor);
+
+        textView = findViewById(R.id.text);
+        scrollView = findViewById(R.id.scroll);
+        markdownView = findViewById(R.id.markdown);
+
+        accept = findViewById(R.id.accept);
+        edit = findViewById(R.id.edit);
+
+        layoutSwitcher = findViewById(R.id.layout_switcher);
+        buttonSwitcher = findViewById(R.id.button_switcher);
+
+        WebSettings settings = markdownView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setBuiltInZoomControls(true);
+        settings.setDisplayZoomControls(false);
+    }
+
+    // onRestoreInstanceState
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState)
+    {
+        super.onRestoreInstanceState(savedInstanceState);
+        markdownView.restoreState(savedInstanceState);
+    }
+
+    // onResume
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        // Clear cache
+        markdownView.clearCache(true);
+
+        if (changed)
+            loadMarkdown();
+
+        setVisibility();
+    }
+
+    // onSaveInstanceState
+    @Override
+    protected void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+        markdownView.saveState(outState);
+    }
+
+    // onPause
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+
+        if (changed)
+            save();
+    }
+
+    // onCreateOptionsMenu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+
+        searchItem = menu.findItem(R.id.search);
+
+        // Set up search view
+        if (searchItem != null)
+            searchView = (SearchView) searchItem.getActionView();
+
+        // Set up search view options and listener
+        if (searchView != null)
+        {
+            searchView.setSubmitButtonEnabled(true);
+            searchView.setImeOptions(EditorInfo.IME_ACTION_GO);
+            searchView.setOnQueryTextListener(new QueryTextListener());
+        }
+
+        return true;
+    }
+
+    // onOptionsItemSelected
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+        case android.R.id.home:
+            onBackPressed();
+            break;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+
+        // Close text search
+        if (searchItem.isActionViewExpanded())
+            searchItem.collapseActionView();
+
+        return true;
+    }
+
+    // onBackPressed
+    @Override
+    public void onBackPressed()
+    {
+        // External
+        else
+        {
+            if (markdownView.canGoBack())
+                markdownView.goBack();
+
+            else
+                super.onBackPressed();
+        }
+    }
+
+    private void setListeners()
+    {
+        if (textView != null)
+            textView.addTextChangedListener(new TextWatcher()
+        {
+            // afterTextChanged
+            @Override
+            public void afterTextChanged(Editable s)
+            {
+                // Text changed
+                changed = true;
+            }
+
+            // beforeTextChanged
+            @Override
+            public void beforeTextChanged(CharSequence s,
+                                          int start,
+                                          int count,
+                                          int after)
+            {
+            }
+
+            // onTextChanged
+            @Override
+            public void onTextChanged(CharSequence s,
+                                      int start,
+                                      int before,
+                                      int count)
+            {
+            }
+        });
+
+        if (markdownView != null)
+        {
+            // On long click
+            markdownView.setOnLongClickListener(v ->
+            {
+                // Reveal button
+                edit.setVisibility(View.VISIBLE);
+                return false;
+            });
+        }
+
+        if (accept != null)
+        {
+            // On click
+            accept.setOnClickListener(v ->
+            {
+                // Check flag
+                if (changed)
+                {
+                    // Save text
+                    save();
+                    // Get text
+                    loadMarkdown();
+                    // Clear flag
+                    changed = false;
+                    // Set flag
+                    entry = true;
+                }
+
+                // Animation
+                animateAccept();
+
+                // Close text search
+                if (searchItem.isActionViewExpanded())
+                    searchItem.collapseActionView();
+
+                shown = true;
+            });
+
+            // On long click
+            accept.setOnLongClickListener(v ->
+            {
+                // Hide button
+                v.setVisibility(View.INVISIBLE);
+                return true;
+            });
+        }
+
+        if (edit != null)
+        {
+            // On click
+            edit.setOnClickListener(v ->
+            {
+                // Animation
+                animateEdit();
+
+                // Close text search
+                if (searchItem.isActionViewExpanded())
+                    searchItem.collapseActionView();
+
+                // Scroll after delay
+                edit.postDelayed(() ->
+                {
+                    // Get selection
+                    int selection = textView.getSelectionStart();
+
+                    // Get text position
+                    int line = textView.getLayout().getLineForOffset(selection);
+                    int position = textView.getLayout().getLineBaseline(line);
+
+                    // Scroll to it
+                    int height = scrollView.getHeight();
+                    scrollView.smoothScrollTo(0, position - height / 2);
+                }, POSITION_DELAY);
+
+                shown = false;
+            });
+
+            // On long click
+            edit.setOnLongClickListener(v ->
+            {
+                // Hide button
+                v.setVisibility(View.INVISIBLE);
+                return true;
+            });
+        }
+
+        if (textView != null)
+        {
+            // onFocusChange
+            textView.setOnFocusChangeListener((v, hasFocus) ->
+            {
+                // Hide keyboard
+                InputMethodManager imm = (InputMethodManager)
+                    getSystemService(INPUT_METHOD_SERVICE);
+                if (!hasFocus)
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+            });
+
+            // On long click
+            textView.setOnLongClickListener(v ->
+            {
+                // Reveal button
+                accept.setVisibility(View.VISIBLE);
+                return false;
+            });
+        }
+    }
+
+    // animateAccept
+    public void animateAccept()
+    {
+        // Animation
+        layoutSwitcher.setDisplayedChild(MARKDOWN);
+        buttonSwitcher.setDisplayedChild(EDIT);
+    }
+
+    // animateEdit
+    private void animateEdit()
+    {
+        // Animation
+        layoutSwitcher.setDisplayedChild(EDIT_TEXT);
+        buttonSwitcher.setDisplayedChild(ACCEPT);
+    }
+
+    // loadMarkdown
+    private void loadMarkdown()
+    {
+        CharSequence text = textView.getText();
+        loadMarkdown(text);
+    }
+
+    // loadMarkdown
+    private void loadMarkdown(CharSequence text)
+    {
+        markdownView.loadMarkdown(getBaseUrl(), markdownCheck(text),
+                                  getStyles(), getScript());
+    }
+
+    // getBaseUrl
+    private String getBaseUrl()
+    {
+        return Uri.fromFile(getCurrent()).toString() + File.separator;
+    }
+
+    // getCurrent
+    private File getCurrent()
+    {
+        return getMonth(currEntry.get(Calendar.YEAR),
+                        currEntry.get(Calendar.MONTH));
+    }
+
+    // getStyles
+    private String getStyles()
+    {
+        File cssFile = new File(getHome(), CSS_STYLES);
+
+        if (cssFile.exists())
+            return Uri.fromFile(cssFile).toString();
+
+        return STYLES;
+    }
+
+    // getScript
+    private String getScript()
+    {
+        File jsFile = new File(getHome(), JS_SCRIPT);
+
+        if (jsFile.exists())
+            return Uri.fromFile(jsFile).toString();
+
+        return null;
+    }
+
+    // setVisibility
+    private void setVisibility()
+    {
+        if (markdown)
+        {
+            buttonSwitcher.setVisibility(View.VISIBLE);
+            // Check if shown
+            if (shown)
+            {
+                layoutSwitcher.setDisplayedChild(MARKDOWN);
+                buttonSwitcher.setDisplayedChild(EDIT);
+            }
+            else
+            {
+                layoutSwitcher.setDisplayedChild(EDIT_TEXT);
+                buttonSwitcher.setDisplayedChild(ACCEPT);
+            }
+        }
+        else
+        {
+            layoutSwitcher.setDisplayedChild(EDIT_TEXT);
+            buttonSwitcher.setVisibility(View.GONE);
+        }
+    }
+
+    // editStyles
+    public void editStyles()
+    {
+        File file = new File(getHome(), CSS_STYLES);
+
+        // Get file provider uri
+        Uri uri = FileProvider.getUriForFile
+            (this, "org.billthefarmer.diary.fileprovider", file);
+        if (BuildConfig.DEBUG)
+            Log.d(TAG, "Path " + uri.getPath());
+
+        Intent intent = new Intent(Intent.ACTION_EDIT);
+        intent.setDataAndType(uri, TEXT_CSS);
+        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION |
+                        Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        startActivity(intent);
+    }
+
+    // editScript
+    public void editScript()
+    {
+        File file = new File(getHome(), JS_SCRIPT);
+
+        // Get file provider uri
+        Uri uri = FileProvider.getUriForFile
+            (this, "org.billthefarmer.diary.fileprovider", file);
+        if (BuildConfig.DEBUG)
+            Log.d(TAG, "Path " + uri.getPath());
+
+        Intent intent = new Intent(Intent.ACTION_EDIT);
+        intent.setDataAndType(uri, TEXT_JAVASCRIPT);
+        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION |
+                        Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        startActivity(intent);
     }
 }
