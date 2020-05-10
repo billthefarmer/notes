9b8508398369aa67c60f66c30bba6a76bc1b2f52
Bill Farmer <williamjfarmer@yahoo.co.uk>
2020-05-10 09:52:36 +0100

    Update copyright

--- src/main/java/org/billthefarmer/notes/Editor.java
+++ src/main/java/org/billthefarmer/notes/Editor.java
@@ -17,6 +17,8 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
+//  Bill Farmer	 william j farmer [at] yahoo [dot] co [dot] uk.
+//
 ////////////////////////////////////////////////////////////////////////////////
 
 package org.billthefarmer.notes;
