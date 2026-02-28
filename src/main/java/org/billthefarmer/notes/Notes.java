a9c9b6f97ec0ffe7db78832c7e549e04d2da155b
Bill Farmer <williamjfarmer@yahoo.co.uk>
2026-02-28 14:51:35 +0000

    Update permissions

--- src/main/java/org/billthefarmer/notes/Notes.java
+++ src/main/java/org/billthefarmer/notes/Notes.java
@@ -379,6 +379,7 @@ public class Notes extends Activity
         }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+            Build.VERSION.SDK_INT < Build.VERSION_CODES.R &&
             checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
             != PackageManager.PERMISSION_GRANTED)
         {
